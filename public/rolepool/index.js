5158496ec0da0f84b74abe9183c4eb69b8f881d8
粥不够多 <2942738629@qq.com>
2026-06-06 06:48:59 +0800

    角色管理界面结构完成

---
+++ public/rolepool/index.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    // 同步角色
+    $('#btn-sync').on('click', function () {
+        // TODO: 调用同步接口
+    });
+
+    // 创建角色
+    $('#btn-create').on('click', function () {
+        // TODO: 跳转创建页或打开弹窗
+    });
+
+});
