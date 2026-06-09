1f24afd40244c4159bde51857229ab0feacc4418
粥不够多 <2942738629@qq.com>
2026-06-10 03:55:52 +0800

    不再使用TypeScript后端

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.static('public'));
 app.use('/static', express.static('static'));
 
-app.listen(4000, () => {
+app.listen(4000, '0.0.0.0', () => {
     console.log('Server is running on http://localhost:4000');
 });
